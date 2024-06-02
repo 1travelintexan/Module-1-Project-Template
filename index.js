c6af1522c0f27b808447332ce2364dd1abb54550
Joshua <78570527+1travelintexan@users.noreply.github.com>
2024-06-02 14:59:17 +0200

    prompts added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
 import * as fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import createDirectoryContents from "./createDirectoryContents.js";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
+import figlet from "figlet";
+import gradient from "gradient-string";
 const CURR_DIR = process.cwd();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -28,13 +33,46 @@ const QUESTIONS = [
     },
   },
 ];
+const sleep = (ms = 2500) => new Promise((r) => setTimeout(r, ms));
+async function ironhackBanner() {
+  const msg = "Iron - Game";
+  figlet(msg, (err, data) => {
+    console.log(gradient.pastel(data));
+  });
+}
+async function welcomeTitle() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    `Welcome Ironhacker! Let's build something cool`
+  );
+  await sleep();
+  rainbowTitle.stop();
+  console.log(chalk.bgBlueBright(`First I need to know a few things`));
+  await sleep();
+}
 
-inquirer.prompt(QUESTIONS).then((answers) => {
+async function prompts() {
+  const answers = await inquirer.prompt(QUESTIONS);
   const projectChoice = answers["project-type-choice"];
   const projectName = answers["project-name"];
   const templatePath = `${__dirname}/templates/${projectChoice}`;
-
   fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+  const spinner = createSpinner("Creating your folder structure...").start();
+  await sleep();
+
+  chalkAnimation.neon(
+    `
+    ***********remember to read all the comments! They are there to help :)***********`
+  );
+  spinner.success({
+    text: `Here is the project template that you requested,
+    Now you can... 
+
+    cd into ${projectName} and start coding :)`,
+  });
 
   createDirectoryContents(templatePath, projectName);
-});
+}
+await ironhackBanner();
+await sleep(1000);
+await welcomeTitle();
+await prompts();
